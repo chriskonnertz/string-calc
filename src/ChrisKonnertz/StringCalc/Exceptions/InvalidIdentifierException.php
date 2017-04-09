5a42092711dd0d017ce18d69b211d785a19fb566
ChrisKonnertz <siconize@gmail.com>
2017-04-09 11:13:43 +0200

    Added InvalidIdentifierException

---
+++ src/ChrisKonnertz/StringCalc/Exceptions/InvalidIdentifierException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ChrisKonnertz\StringCalc\Exceptions;
+
+/**
+ * Class InvalidIdentifier is thrown when validation of an identifier fails.
+ * This exception type is especially used by the AbstractSymbol class.
+ *
+ * @package ChrisKonnertz\StringCalc\Exceptions
+ */
+class InvalidIdentifierException extends \Exception
+{
+
+}
