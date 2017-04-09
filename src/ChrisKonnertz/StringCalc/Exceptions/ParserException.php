2d9597419398a73d0b732b442277ea1181d23f80
ChrisKonnertz <siconize@gmail.com>
2017-04-09 17:12:30 +0200

    Added ParserException

---
+++ src/ChrisKonnertz/StringCalc/Exceptions/ParserException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ChrisKonnertz\StringCalc\Exceptions;
+
+/**
+ * Class Parserxception is thrown when something went wrong during the parsing of the term.
+ *
+ * @package ChrisKonnertz\StringCalc\Exceptions
+ */
+class ParserException extends \Exception
+{
+
+}
