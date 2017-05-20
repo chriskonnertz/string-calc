aaaaf3b152b7c9b66fabdcd63c64661d1191cacc
ChrisKonnertz <siconize@gmail.com>
2017-05-20 18:44:20 +0200

    Ensure PHPUnit backwards compatibility

--- tests/StringCalcTests.php
+++ tests/StringCalcTests.php
@@ -1,9 +1,16 @@
 <?php
 
+// Ensure backward compatibility
+// @see http://stackoverflow.com/questions/42811164/class-phpunit-framework-testcase-not-found#answer-42828632
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 /**
  * Class StringCalcTest for tests with PHPUnit.
  */
-class StringCalcTest extends \PHPUnit_Framework_TestCase
+class StringCalcTest extends PHPUnit_Framework_TestCase
 {
 
     /**
