e20a44ed85fb52b017d7a92fde80e411cc27fef8
Chris Konnertz <chriskonnertz@users.noreply.github.com>
2017-05-21 18:12:52 +0200

    Removed unnecessary space

--- src/ChrisKonnertz/StringCalc/Symbols/Concrete/Functions/EnFunction.php
+++ src/ChrisKonnertz/StringCalc/Symbols/Concrete/Functions/EnFunction.php
@@ -7,7 +7,7 @@ use ChrisKonnertz\StringCalc\Symbols\AbstractFunction;
 /**
  * In StringCalc it is not possible to write this valid PHP number: 2E3
  * which is called "engineering notation" and will evaluate to
- * 2 * pow(10, 3) = 2000.0 (float) in PHP. This function is  a workaround,
+ * 2 * pow(10, 3) = 2000.0 (float) in PHP. This function is a workaround,
  * so instead of "6E12" write "e(2, 3)". Example: "e(2, 3)" => 2000 (int)
  * Attention: As you might have noticed, the result of this function might
  * be an integer while the PHP code always results in a float value!
