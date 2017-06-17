f258dad8ec86ec7f1d1f9ef4fef10391695502bc
ChrisKonnertz <siconize@gmail.com>
2017-06-17 16:56:50 +0200

    Added failing test

--- tests/StringCalcTests.php
+++ tests/StringCalcTests.php
@@ -86,7 +86,7 @@ class StringCalcTest extends \PHPUnit\Framework\TestCase
             ['onePi', M_1_PI],
             ['pi', M_PI],
             ['piFour', M_PI_4],
-            ['piTwo', M_PI_2],
+            ['piTwos', M_PI_2],
             ['sqrtOneTwo', M_SQRT1_2],
             ['sqrtPi', M_SQRTPI],
             ['sqrtThree', M_SQRT3],
