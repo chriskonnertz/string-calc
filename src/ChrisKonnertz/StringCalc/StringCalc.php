a43d3399c5f737c591ee68058490f4e77c8bb5c8
Christoph K <siconize@gmail.com>
2017-08-16 22:54:54 +0200

    Reverted change in demo.php, increased version number

--- src/ChrisKonnertz/StringCalc/StringCalc.php
+++ src/ChrisKonnertz/StringCalc/StringCalc.php
@@ -29,7 +29,7 @@ class StringCalc
      *
      * @const string
      */
-    const VERSION = '1.0.7';
+    const VERSION = '1.0.8';
 
     /**
      * Closure that is called at the end of the grammar checking
